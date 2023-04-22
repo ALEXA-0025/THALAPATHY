0ae49542bbb8b0367a52d16ce6839a5516a11c91
ALEXA-0025 <131378080+ALEXA-0025@users.noreply.github.com>
2023-04-22 10:49:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,6 +38,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+DELETE_TIME = int(environ.get('DELETE_TIME', "9999"))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
